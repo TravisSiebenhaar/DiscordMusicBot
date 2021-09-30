24998970ead8393fb509917fd55f8c28f3724d19
Admin <trsiebenhaar@gmail.com>
2021-09-29 22:14:28 -0600

    Add packages, add base case, add message

--- index.mjs
+++ index.mjs
@@ -1,12 +1,30 @@
 import { Client, Intents } from "discord.js";
 import dotenv from "dotenv";
+import * as config from "./config.js";
 
 dotenv.config();
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
 client.on("ready", () => {
   console.log(`Logged in to ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (
+    message.content.startsWith(config.prefix) ||
+    message.author.bot ||
+    message.channel.name !== "song_request"
+  )
+    return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(" ");
+  const command = args.shift().toLowerCase();
+  console.log("command", command);
+
+  if (command === "ping") {
+    message.channel.send("pong");
+  }
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
